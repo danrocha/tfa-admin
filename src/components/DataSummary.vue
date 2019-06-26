89d64c4b2c2c11851565284e546a68237c9980a5
Daniel da Rocha <danrocha@gmail.com>
2019-06-26 15:08:34 +0200

    create data summary component

---
+++ src/components/DataSummary.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p v-if="architects">{{ architects.totalCount }} architects.</p>
+    <p v-if="buildings">{{ buildings.totalCount }} buildings.</p>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+
+export default {
+  name: 'DataSummary',
+  data() {
+    return {
+      architects: null,
+      buildings: null
+    };
+  },
+  apollo: {
+    architects: {
+      query: gql`
+        query architects {
+          architects {
+            totalCount
+          }
+        }
+      `
+    },
+    buildings: {
+      query: gql`
+        query buildings {
+          buildings {
+            totalCount
+          }
+        }
+      `
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
