d5db28086ec43232cb0618110943660f96c33ed3
Daniel da Rocha <danrocha@gmail.com>
2019-06-26 15:04:35 +0200

    add dev proxy

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,10 @@
-module.exports = {}
+module.exports = {
+  devServer: {
+    proxy: {
+      '/graphql': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
+  }
+};
